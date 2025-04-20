c3f824aa2a692d93b6f0776b3bc49898124ccc61
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-04-20 23:06:36 +0530

    api fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,69 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-
+import ShimmerUi from "./ShimmerUi";
 const Body=()=>{
 
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
  // we can also write the above line as below
   // const arr = useState(resList);
   // const listOfRestaurant = arr[0];
   // const setListOfRestaurant = arr[1];
+
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(()=>{
+    fetchData();
+
+  },[]);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.9680035&lng=77.55520659999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    //this is not a good way of writing it so we need optional chaining like below
+    setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+  if(listOfRestaurant.length === 0) {
+    return <ShimmerUi/>;
+  }
+  // if we do not have any data then we will show the shimmer ui
+
     return(
       <div className="body">
         <div className="filter">
+        <div className="search">
+          <input
+           type="text"
+           className="search-box"
+           value={searchText}
+           onChange={(e)=>{
+            setSearchText(e.target.value);
+
+           }} />
+          <button className="search-btn"
+           onClick={()=>{
+            console.log(searchText);
+            const filteredRestaurant = listOfRestaurant.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              
+            );
+            setFilteredRestaurant(filteredRestaurant);
+           }} 
+          >
+           Search
+          </button>
+        </div>
         <button
         className="filter-btn"
         onClick={() => {
           const filteredList = listOfRestaurant.filter(
-            (res) => res.info.avgRating > 4
+            (res) => res.info.avgRating > 4.5
           );
           setListOfRestaurant(filteredList);
         }}
@@ -28,7 +74,7 @@ const Body=()=>{
         </div>
         <div className="restro-container">
           {/* <RestaurantCard resData = {resList[0]}/> */}
-          {listOfRestaurant.map((restaurant)=>(
+          {filteredRestaurant.map((restaurant)=>(
             <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
             ))}
           
---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,24 @@
+const ShimmerUi=()=>{
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            
+        </div>
+ 
+    );
+};
+export default ShimmerUi;
