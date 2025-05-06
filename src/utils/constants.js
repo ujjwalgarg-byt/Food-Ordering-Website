f96baea4986db68bfa11e24e7c703f03007db058
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-05-06 12:05:03 +0530

    add more hooks or apis

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All";
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=29.9680035&lng=77.55520659999999&restaurantId=";
+
+export const RESTAURANT_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.9680035&lng=77.55520659999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";  
