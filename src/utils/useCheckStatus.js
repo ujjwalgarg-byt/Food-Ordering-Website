0d019c25f169b051f3794566b0497b33cccc722b
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-05-06 12:05:21 +0530

    built new hook

---
+++ src/utils/useCheckStatus.js
@@ -0,0 +1,12 @@
+
+import { useState,useEffect } from "react";
+
+const useCheckStatus = () => {
+const [onlineStatus,setOnlineStatus] = useState(true);
+    useEffect(()=>{
+    window.addEventListener("offline",()=>setOnlineStatus(false));
+    window.addEventListener("online",()=>setOnlineStatus(true));
+    },[]);
+    return onlineStatus;
+}
+export default useCheckStatus;
