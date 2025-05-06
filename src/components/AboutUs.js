705c1333c19211369a98c7c99eb629648b85ba2d
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-05-06 12:03:20 +0530

    adding user's details

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,13 @@
+import { json } from "react-router-dom";
+import User from "./User";
+import UserClass from "./UserClass";
 const AboutUs = () => {
     return (
         <div className="about-us">
-            <h1>aboutus</h1>
+            <h1>About Us</h1>
+            {/* <User name = {"Ujjwal garg"}/> */}
+            <h3>Find User's details here</h3>
+            <UserClass />
         </div>
     )
 };
