d51cae655b0828cebed4a95228105d70bbd0a721
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-05-08 20:07:13 +0530

    add some styles and features

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,7 +3,7 @@ import User from "./User";
 import UserClass from "./UserClass";
 const AboutUs = () => {
     return (
-        <div className="about-us">
+        <div className="about-us m-4 p-4 bg-[#FAF6E9]">
             <h1>About Us</h1>
             {/* <User name = {"Ujjwal garg"}/> */}
             <h3>Find User's details here</h3>
