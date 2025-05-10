ce27902f75572a5c2771f9aa0b4c7fcb3e9d0382
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-05-10 22:05:26 +0530

    add new items & features

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,16 @@ const Header=()=>{
     return (
      <div className="flex justify-between border-1 border-black bg-[#FFFA8D]  p-2 shadow-lg">
       <div className="logo-container">
-        <img className=" w-24 rounded-lg shadow-lg" src={LOGO_URL}/>
+        <img className=" w-24 rounded-xl shadow-lg" src={LOGO_URL}/>
       </div>
       <div className="flex  items-center">
         <ul className="flex ">
-          <li className="p-2.5 m-2.5">Online Status:{onlineStatus ? "🟢" :"🔴"}</li>
-          <li className="p-2.5 m-2.5"><Link to="/" style={{ textDecoration: 'none', color:"black" }}>Home</Link></li>
-          <li className="p-2.5 m-2.5"><Link to="/about" style={{ textDecoration: 'none', color:"black" }}>About Us</Link></li>
-          <li className="p-2.5 m-2.5"><Link to="/contact" style={{ textDecoration: 'none', color:"black" }}>Contact Us</Link></li>
-          <li className="p-2.5 m-2.5">Cart</li>
+          <li className="p-2.5 m-2.5 font-semibold">Online Status:{onlineStatus ? "🟢" :"🔴"}</li>
+          <li className="p-2.5 m-2.5 font-semibold"><Link to="/" >Home <i className="fa-solid fa-house"></i></Link></li>
+          
+          <li className="p-2.5 m-2.5 font-semibold"><Link to="/about" >About Us <i className="fas fa-users"></i></Link></li>
+          <li className="p-2.5 m-2.5 font-semibold"><Link to="/contact" >Contact Us <i className="fas fa-envelope"></i></Link></li>
+          <li className="p-2.5 m-2.5 font-semibold"><Link to="/cart">Cart <i className="fa-solid fa-cart-shopping"></i></Link></li>
           <button className="login bg-[#F6EFBD] p-2.5 m-2.5 rounded-lg border border-solid border-black cursor-pointer"  
             onClick={()=>{
             btnName==="LogIn"
