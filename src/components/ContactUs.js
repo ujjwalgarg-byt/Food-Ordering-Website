21ac7b374fdee52f70b61d41a34ecb528c5e6938
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-05-10 22:05:14 +0530

    add new items & features

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,8 @@
 const ContactUs=()=>{
     return (
-        <div className="Contact">Contact Us</div>
+        <div className="Contact flex justify-center items-center h-screen bg-[#A8F1FF]">
+            <h1>This is Contact Page</h1>
+        </div>
 
     )
 }
